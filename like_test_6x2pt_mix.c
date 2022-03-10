3d86d0ed10e7709ef2829900f920db7617dd6c3f
Vivian Miranda <vivianmiranda@arizona.edu>
2022-03-09 22:27:35 -0700

    update

--- like_test_6x2pt_mix.c
+++ like_test_6x2pt_mix.c
@@ -2,6 +2,7 @@
 
 void test_mix_desy1_planck()
 {
+	like.shearcalib=1;
 	// parameter settings start
 	double theta_min = 2.5, theta_max = 250.0;
 	int Ntheta = 20;
@@ -12,7 +13,7 @@ void test_mix_desy1_planck()
 	double lmax_kappacmb = 2999.0;
 	double ggl_cut = 0.0;
 	int ntomo_source = 4, ntomo_lens = 5;
-	char runmode[50] = "Halofit";
+	char runmode[50] = "class";
 	char source_nz[500] = "./zdistris/mcal_1101_source.nz";
 	char lens_nz[500] = "./zdistris/mcal_1101_lens.nz";
 	char probes[50] = "6x2pt";
@@ -25,19 +26,29 @@ void test_mix_desy1_planck()
 	int IA_model = 4; // 4 = NLA
 
 	double b2[10] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	// parameter setting end
+
+	clock_t begin, end;
+	double time_spent;
+	int i;
+	begin = clock();
+
+	// Initialization
+	init_cosmo_runmode(runmode);
+
 	input_cosmo_params_y3 ic = {
 		.omega_m = 0.3,
-		.sigma_8 = 0.827726,
+		.sigma_8 = 0.842911,
 		//.A_s = 2.1e-9,
 		.n_s = 0.96605,
 		.w0 = -1.0,
 		.wa = 0.0,
 		.omega_b = 0.04,
-		.omega_nuh2 = 0.06/93.14,
+		.omega_nuh2 = 0.0,
 		.h0 = 0.6732,
 		.MGSigma = 0.0,
 		.MGmu = 0.0,
-		//.theta_s = -1.0,
+		//.theta_s = 0.0104854,
 	};
 	input_nuisance_params_y3 in = {
 		.bias = {1.72716, 1.65168, 1.61423, 1.92886, 2.11633, 
@@ -50,17 +61,11 @@ void test_mix_desy1_planck()
 			0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
 		.shear_m = {0.0191832, -0.0431752, -0.034961, -0.0158096, 
 			0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-		.p_ia = {0.606102, -1.51541, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+		.p_ia = {0.6061028, -1.51541, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
 	};
-	// parameter setting end
 
-	clock_t begin, end;
-	double time_spent;
-	int i;
-	begin = clock();
 
-	// Initialization
-	init_cosmo_runmode(runmode);
+
 	init_source_sample_mpp(source_nz, ntomo_source);
 	init_lens_sample_mpp(lens_nz, ntomo_lens, in.bias, b2, ggl_cut);
 	init_binning_fourier(Ncl, l_min, l_max);
@@ -70,9 +75,9 @@ void test_mix_desy1_planck()
 	init_cmb(cmbName, cmb_lens_noise_file);
 	init_data_fourier(cov_file, mask_file, data_file);
 	init_IA_mpp(IA_model);
-	sprintf(survey.name,"%s","DESY1xplanck");
-
+	sprintf(survey.name,"%s","DESY1xplanck");;
 	// calculate and write model vector
+	printf("test3\n\n");
 	write_datavector_wrapper(test_model_file, ic, in);
 	printf("model vector written to %s\n", test_model_file);
 
